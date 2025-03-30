237e7d41cee8cfc4131ba5e815cd30f301742811
Flávio Gabriel Batista Oliveira <flavio.batista1313@gmail.com>
2025-03-30 17:25:06 -0300

    first commit

---
+++ PROJETO/projeto.py
@@ -0,0 +1,18 @@
+# Cadastro da senha
+senha = input("Digite uma senha para o celular: ")
+
+# Tentativas de acesso
+tentativas = 3
+
+while tentativas > 0:
+    senha_inserida = input("Digite a senha para acessar o celular: ")
+    
+    if senha_inserida == senha:
+        print("Bem-vindo!")
+        break
+    else:
+        tentativas -= 1
+        print(f"Senha incorreta. Você tem {tentativas} tentativas até o bloqueio.")
+        
+if tentativas == 0:
+    print("O celular foi bloqueado.")
